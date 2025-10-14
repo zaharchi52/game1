ee60052c9527f9f4c77ffdc9d3e4f86f27aea09a
zaharchi52 <petrovzax@gmail.com>
2025-10-15 00:32:47 +0300

    создал игрока, добавил спрайт, сделал подобие стены и пола, на них можно будет добавить спрайты

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [TextArea] public string promptText = "Нажми E чтобы взаимодействовать";
+    public bool canInteract = true;
+
+    // Этот метод вызывается при нажатии E игроком
+    public virtual void Interact()
+    {
+        Debug.Log("Interact with " + name);
+    }
+}
